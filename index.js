332d872f956fef8dfc2e8cb9956386467af13543
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-18 11:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,13 +19,43 @@ function add2(n) {
 
 }
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
-}
+/*
+/*global describe, it */
+/*
+describe('Fix the Scope', function() {
+  describe('myAnimal()', function() {
+    it('returns my animal', () => {
+      expect(window.myAnimal()).toEqual('dog')
+    })
+  })
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-function funkyFunction(){return "FUNKY!"}
-var theFunk = funkyFunction()
+  describe('yourAnimal()', function() 
+    it('returns your animal', function(){
+      expect(window.yourAnimal()).toEqual('cat')
+    })
+
+    it('does not hard-code the answer', function() {
+      expect(window.yourAnimal.toString()).toNotContain("return 'cat'")
+    })
+  })
+
+  describe('add2(n)', function() {
+    it('adds two to n', function() {
+      const n = Math.floor(Math.random() * 1000)
+      expect(window.add2(n)).toEqual(n + 2)
+    })
+  })
+
+  describe('funkyFunction()', function() {
+    it('returns a function', function() {
+      expect(typeof window.funkyFunction()).toEqual('function')
+    })
+
+  describe('theFunk', function() {
+    it('is "FUNKY!"', function() {
+      expect(window.theFunk).toEqual('FUNKY!')
+    })
+  })
+  */
+
+  })
